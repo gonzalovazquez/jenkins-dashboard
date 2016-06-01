fc413880c5dd0ca57c62d3ec9cc47e9c027e3f50
gonzalovazquez <gonzalo.segura1@gmail.com>
2016-05-31 20:13:41 -0400

    adding observable to gather data

--- src/app/jenkins-dashboard.component.ts
+++ src/app/jenkins-dashboard.component.ts
@@ -4,6 +4,7 @@ import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 import { Job } from './job';
 import {JenkinsDashboardService} from './jenkins-dashboard.service';
 import { OnInit } from '@angular/core';
+import './rxjs-operators';
 
 @Component({
   moduleId: module.id,
@@ -16,11 +17,15 @@ import { OnInit } from '@angular/core';
 export class JenkinsDashboardAppComponent implements OnInit {
   title = 'Jenkins Dashboard';
   jobs:Job[];
+  errorMessage: string;
     
   constructor(private jenkinsService: JenkinsDashboardService) { }
   
   getJobs() {
-    this.jenkinsService.getJobs().then(jobs => this.jobs = jobs);
+    this.jenkinsService.getJobs()
+                        .subscribe(
+                          jobs => this.jobs = jobs,
+                          error => this.errorMessage = <any>error);
   }
   
   ngOnInit() {
--- src/app/jenkins-dashboard.service.ts
+++ src/app/jenkins-dashboard.service.ts
@@ -1,10 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
 import { Job } from './job';
-import { JOBS } from './mock-jenkins';
+import { Observable } from 'rxjs/Observable';
+//import { JOBS } from './mock-jenkins';
 
 @Injectable()
 export class JenkinsDashboardService {
-    getJobs(){
-        return Promise.resolve(JOBS);
+    constructor(private http:Http) {}
+    
+    private jenkinsURL = 'https://jenkins-dashboard-api.herokuapp.com/job/Libraries';  // URL to web API
+    
+    getJobs (): Observable<Job[]> {
+        return this.http.get(this.jenkinsURL)
+                    .map(this.extractData)
+                    .catch(this.handleError);
     }
+    
+    private extractData(res: Response) {
+        let body = res.json();
+        console.log(body);
+        return body.data || { };
+    }
+    
+    private handleError (error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg); // log to console instead
+        return Observable.throw(errMsg);
+     }
+  
 }
